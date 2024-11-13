54b0b10e4190b1725ce7809ddf85f54eda668bc7
oleg <oleh.naumov@techmagic.co>
2024-11-13 16:45:44 +0200

    fix(gitignore): removed  types.d.ts form  gitignore

---
+++ src/types.d.ts
@@ -0,0 +1,8 @@
+declare module 'passkit-node-sdk/io/member/member_pb';
+declare module 'passkit-node-sdk/io/common/personal_pb';
+declare module 'passkit-node-sdk/io/member/a_rpc_grpc_pb';
+declare module 'passkit-node-sdk/io/core/a_rpc_images_grpc_pb';
+declare module 'passkit-node-sdk/io/common/filter_pb';
+declare module 'class-transformer/cjs/storage';
+declare module 'passkit-node-sdk/io/common/common_objects_pb';
+declare module 'passkit-node-sdk/io/image/image_pb';
